732e5eb50a95646e859f29ba9b38a8792e3a182a
Dimitri <dimadening@web.de>
2020-05-26 17:47:18 +0200

    delete files

--- liquid-level-detection/foo.m
+++
@@ -1,9 +0,0 @@
-%% boundaries
-hold on;
-boundaries = bwboundaries(binaryImage);
-numberOfBoundaries = size(boundaries, 1);
-for k = 1 : numberOfBoundaries
-	thisBoundary = boundaries{k};
-	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
-end
-hold off;
